f534dca6d4b3d5a08886fa7e643226a98603e640
Jaqqen <hussain.daniyal@outlook.com>
2019-09-19 20:47:07 +0200

    bugfix/SV-4_Fixing_issue_after_identifcation_canvas_unmounts
    trying to resolve the 'set-value'-dependency security alert by creating a new package-lock.json and yarn.lock with updated package.json-dependencies

---
+++ frontend/src/shared/Fetcher.js
@@ -0,0 +1,22 @@
+const checkStatus = response => {
+  if (response.ok) {
+    return response;
+  } else {
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+};
+
+const parseJSON = res => res.json();
+
+const Fetcher = {
+  get: (path, params) => {
+    fetch(path, params)
+      .then(checkStatus)
+      .then(parseJSON)
+  },
+  post: () => {
+
+  },
+}
